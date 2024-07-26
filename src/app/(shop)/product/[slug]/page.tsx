ca0406a3535080119fc266254dfd0483523c3d88
Anderson VT arco <andersonventosilla0@gmail.com>
2024-07-26 18:52:35 -0500

    209

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,56 @@
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
 
-export default function () {
+interface Props {
+  params: {
+    slug: string;
+  }
+}
+
+export default function ({ params }: Props) {
+
+  const { slug } = params;
+  const product = initialData.products.find(product => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+
+      {/* slideShow */}
+      <div className="col-span-1 md:col-span-2">
+        <h1>Hola mundo</h1>
+      </div>
+
+      {/* {detalles} */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
+          {product.title}
+        </h1>
+        <p className="text-lg mb-5">${product.price}</p>
+
+        {/* selector de tallas */}
+
+
+        {/* selector de cantidad */}
+
+
+        {/* button */}
+        <button className="btn-primary my-5">
+          Agregar al carrito
+        </button>
+
+        {/* descripcion */}
+
+        <h3 className="font-bold text-sm">Descripcion</h3>
+        <p className="font-light">{product.description}</p>
+
+      </div>
+
     </div>
   );
 }
