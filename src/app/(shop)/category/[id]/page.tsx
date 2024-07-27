79901eded10b609ee0b0041f7c529329586d0f55
Anderson <andersonventosilla0@gmail.com>
2024-07-26 20:40:56 -0500

    diseño teslo shop added

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,24 @@
+import { notFound } from "next/navigation";
 
-export default function () {
+interface Props {
+  params: {
+    id: string;
+  }
+}
+
+export default function ({ params }: Props) {
+
+  const { id } = params
+
+  console.log(id)
+
+  if(id === 'kids'){
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page {id}</h1>
     </div>
   );
 }
