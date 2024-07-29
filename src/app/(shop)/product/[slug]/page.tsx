cca65786730e5fff291d5607541eb57a849fb1c5
Anderson <andersonventosilla0@gmail.com>
2024-07-28 21:24:49 -0500

    diseño parte 2 hecho

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
+import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
 import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
@@ -20,13 +20,22 @@ export default function ({ params }: Props) {
 
 
   return (
-    <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
 
       {/* slideShow */}
       <div className="col-span-1 md:col-span-2">
-        <ProductSlideshow 
+        {/* Mobile slideshow */}
+        <ProductMobileSlideshow
           title={product.title}
           images={product.images}
+          className="block md:hidden"
+        />
+
+        {/* Desktop slideshow  */}
+        <ProductSlideshow
+          title={product.title}
+          images={product.images}
+          className="hidden md:block"
         />
       </div>
 
@@ -44,7 +53,7 @@ export default function ({ params }: Props) {
         />
 
         {/* selector de cantidad */}
-        <QuantitySelector quantity={2}/>
+        <QuantitySelector quantity={2} />
 
         {/* button */}
         <button className="btn-primary my-5">
